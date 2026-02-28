6d7e61ea14f2e1455e43e6e97b50585f6dfa1ba4
unknown <hridays2005@gmail.com>
2026-02-28 15:50:23 +0530

    added initial difference section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,16 @@
+import Difference from "@/components/About/Difference";
+import Hero from "@/components/About/Hero";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 import React from "react";
 
 const page = () => {
   return (
     <div className="flex flex-col w-full min-h-screen items-center justify-start px-16 bg-background font-sans dark:bg-black gap-16">
-      page
+      <Navbar />
+      <Hero />
+      <Difference />
+      <Footer />
     </div>
   );
 };
--- components/About/Hero.tsx
+++ components/About/Hero.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="flex flex-col w-full h-fit justify-center items-center gap-9 py-6 max-w-section">
+      <p className="w-fit h-fit text-[#F7F9FA]/50 text-sm font-inter">Home / About</p>
+      <h1 className="text-[64px] text-text-primary font-sora font-bold text-center">
+        About Coderush
+      </h1>
+      <p className="w-[60%] h-fit text-lg leading-[1.6em] tracking-[-0.02em] text-text-secondary font-inter text-center">
+        We’re a student-led tech community focused on building, learning, and
+        growing together.
+      </p>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
